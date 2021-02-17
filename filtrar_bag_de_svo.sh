7dd950712d482cbe7ad89d64a5f067416c8b1665
Cris <cristopher.gomez@ug.uchile.cl>
2021-02-17 18:37:18 -0300

    modificaciones

--- filtrar_bag_de_svo.sh
+++ filtrar_bag_de_svo.sh
@@ -1,1 +1,1 @@
-rosbag filter $1 filtrado_$1 "topic=='/dvs/events' or topic=='/dvs/camera_info' or topic=='/svo/pose_cam/0'"
+rosbag filter $1 filtrado_$1 "topic=='/dvs/events' or topic=='/dvs/camera_info' or topic=='/svo/pose_cam/0' or topic=='/dvs/image_raw'"
--- filtrar_bag_de_svo_eventos.sh
+++
@@ -1,1 +0,0 @@
-rosbag filter $1 filtrado_$1 "topic=='/dvs/events' or topic=='/dvs/camera_info' or topic=='/svo/pose_cam/0'"
