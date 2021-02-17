f2ba48772761fe0617fbbbf0bcc4ad456385f982
Cris <cristopher.gomez@ug.uchile.cl>
2021-02-17 18:37:05 -0300

    se agregan scripts

--- calibracion/add_cam_info.sh
+++ calibracion/add_cam_info.sh
@@ -1,1 +1,1 @@
-python add_camera_info.py --inbag $1 --outbag $1.cam_info --replacement /dvs/image_raw=davis-cal.yaml
+python add_camera_info_from_kalibr.py --inbag $1 --outbag $1.cam_info --replacement /dvs/image_raw=/home/tof/Desktop/kalibr-cal/out-camchain/camchain-aruco-tablero-grande-filter.yaml
